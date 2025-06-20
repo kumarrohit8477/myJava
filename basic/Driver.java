f079f12d2c3c448df22f0489652e4f5a33a12bf1
kumarrohit8477 <kumarrohitt0976@gmail.com>
2025-06-21 00:07:52 +0530

    Create Driver.java

---
+++ basic/Driver.java
@@ -0,0 +1,20 @@
+public class Driver {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.drive();
+        myCar.getCurrentFuelInLiters();
+        myCar.addFuel(8);
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+
+    }
+}
