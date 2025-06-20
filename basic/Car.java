e8cc0d2bd315280437379bc4c8ab46d9dd7682d4
kumarrohit8477 <kumarrohitt0976@gmail.com>
2025-06-21 00:07:21 +0530

    Update Car.java

--- basic/Car.java
+++ basic/Car.java
@@ -1,15 +1,46 @@
 public class Car {
 
-    // Method to display car details
-    public void drive() {
-        System.out.println("The car is driving.");
+    // instace variables
+    static int carSold = 500;
+    int noOfWheels;
+    int noOfDoors;
+    int noOfSeats;
+    String color;
+    String Brand;
+    float maxSpeed;
+    float currentFuelInLiters =1;
+    int price;
+
+
+    // instace methods
+   
+    public void start(){
+        if (currentFuelInLiters <= 0) {
+            System.out.println("Can't start no fuel left.");     
+        }
+
+        else if (currentFuelInLiters<=5) {
+            System.out.println("Warning: Low fuel level. Please refuel soon.");
+            System.out.println("The car is startig, bruh....");
+        }
+        else{
+            System.out.println("The car is startig, bruh....");  
+        }
     }
 
-    public static void main(String[] args) {
-        // Create a new instance of the Scanner class
-        Car myCar = new Car();
-        // Call the method to display car details
-        myCar.drive();
+     public void drive(){
+        currentFuelInLiters--;
+        System.out.println("The car is driving.");     
+    }
 
+
+    public void getCurrentFuelInLiters(){
+        System.out.println("Current fuel in car is " + currentFuelInLiters + " liters.");
+    }
+    
+
+    public void addFuel(float fuelInLiters){
+        currentFuelInLiters += fuelInLiters;
+        System.out.println("Added " + fuelInLiters + " liters of fuel. Current fuel level: " + currentFuelInLiters + " liters.");
     }
 }
