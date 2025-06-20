8ee8724aa027529469bbe9aaf254ae8fe15ffe6f
kumarrohit8477 <kumarrohitt0976@gmail.com>
2025-06-20 19:35:15 +0530

    Create Car.java

---
+++ basic/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+
+    // Method to display car details
+    public void drive() {
+        System.out.println("The car is driving.");
+    }
+
+    public static void main(String[] args) {
+        // Create a new instance of the Scanner class
+        Car myCar = new Car();
+        // Call the method to display car details
+        myCar.drive();
+
+    }
+}
