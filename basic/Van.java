b0dae10b10e6f4620cc201e4c3dff5d13412cdee
kumarrohit8477 <kumarrohitt0976@gmail.com>
2025-06-21 00:07:39 +0530

    Create Van.java

---
+++ basic/Van.java
@@ -0,0 +1,15 @@
+public class Van {
+
+    // Method to display car details
+    public void drive() {
+        System.out.println("The car is driving.");
+    }
+
+    public static void main(String[] args) {
+        // Create a new instance of the Scanner class
+        Van myCar = new Van();
+        // Call the method to display car details
+        myCar.drive();
+
+    }
+}
